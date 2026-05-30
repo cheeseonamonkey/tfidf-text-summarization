418974b017e8fc54e9d6d1a8c1d7974716d19288
alexander <you@example.com>
2026-05-30 01:53:44 -0600

    Fix two runtime bugs: missing import re and load_corpus reading filesystem

--- tfidf.py
+++ tfidf.py
@@ -1,4 +1,4 @@
-import sys
+import re
 
 import nltk
 from nltk.stem.porter import *
@@ -8,7 +8,6 @@ from collections import Counter
 import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 import zipfile
-import os
 
 
 def gettext(xmltext) -> str:
@@ -120,9 +119,6 @@ def load_corpus(zipfilename: str) -> dict:
     with zipfile.ZipFile(zipfilename, mode="r") as archive:
         for filename in archive.namelist():
             if filename.endswith(".xml"):
-                text = ""
-                with open(filename, "r") as f:
-                    for line in f:
-                        text += str(line)
+                text = archive.read(filename).decode("ascii", errors="ignore")
                 return_dict[filename.split("/")[1]] = text
     return return_dict
